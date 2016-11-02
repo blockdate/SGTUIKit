743b4178a6e59ff15c485d3732d1c1deff4579fb
吴磊 <869938879@qq.com>
2016-11-02 14:40:10 +0800

    fix some warning and some bug

--- Source/Category/UIKit/UIImage+SGTExt.h
+++ Source/Category/UIKit/UIImage+SGTExt.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "UIImage+SGTExt.h"
 
 @interface UIImage (SGTExt)
 
--- Source/Category/UIKit/UINavigationBar+SGTExt.m
+++ Source/Category/UIKit/UINavigationBar+SGTExt.m
@@ -9,32 +9,24 @@
 #import "UINavigationBar+SGTExt.h"
 #import <objc/runtime.h>
 #import "SGTGlobalDefine.h"
+#import "UIImage+SGTExt.h"
 
 @implementation UINavigationBar (BackgroundColor)
-static char overlayKey;
+//static char overlayKey;
 
-- (UIView *)overlay
-{
-    return objc_getAssociatedObject(self, &overlayKey);
-}
-
-- (void)setOverlay:(UIView *)overlay
-{
-    objc_setAssociatedObject(self, &overlayKey, overlay, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-}
+//- (UIView *)overlay
+//{
+//    return objc_getAssociatedObject(self, &overlayKey);
+//}
+//
+//- (void)setOverlay:(UIView *)overlay
+//{
+//    objc_setAssociatedObject(self, &overlayKey, overlay, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+//}
 
 - (void)lt_setBackgroundColor:(UIColor *)backgroundColor
 {
-    if (!self.overlay) {
-        [self setBackgroundImage:[UIImage new] forBarMetrics:UIBarMetricsDefault];
-        
-        // insert an overlay into the view hierarchy
-        self.overlay = [[UIView alloc] initWithFrame:CGRectMake(0, -20, [UIScreen mainScreen].bounds.size.width, self.bounds.size.height + 20)];
-        self.overlay.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
-        
-        [self insertSubview:self.overlay atIndex:0];
-    }
-    self.overlay.backgroundColor = backgroundColor;
+    [self setBackgroundImage:[UIImage imageFromColor:backgroundColor size:CGSizeMake([UIScreen mainScreen].bounds.size.width, 64)] forBarMetrics:UIBarMetricsDefault];
 }
 
 - (void)lt_setbottomLineHide:(BOOL)hide {
